4cd4aa61a340a94bbd1f8ee45e50f84587bea553
Libasse MBAYE <moneu01@gmail.com>
2022-05-12 16:40:14 +0100

    Second ViewController

--- PodRemote/Welcome.swift
+++ PodRemote/Welcome.swift
@@ -16,8 +16,20 @@ class Welcome: UIViewController {
         // Do any additional setup after loading the view.
         
         self.view.backgroundColor = .brown
+        
+        let but = UIButton()
+        but.frame = CGRect(x: 80, y: 250, width: 180, height: 50)
+        but.backgroundColor = .white
+        but.setTitle("Open", for: .normal)
+        but.setTitleColor(.blue, for: .normal)
+        self.view.addSubview(but)
+        but.addTarget(self, action: #selector(openSecondVC), for: .touchUpInside)
     }
 
 
+    @objc func openSecondVC(){
+        
+        let vc = SecondVC()
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
-
