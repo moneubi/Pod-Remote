afb4336d72e1a26761eda7a703ca379ab2c651d3
Libasse MBAYE <moneu01@gmail.com>
2022-05-12 14:47:43 +0100

    Initial Commit

---
+++ PodRemote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodRemote
+//
+//  Created by DIFA SGMA on 12/5/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
